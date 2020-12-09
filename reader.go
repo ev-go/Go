0c125f8cef4732770cbb75f7ae5bfd4f6cdf625a
ev <dmitry_dtv@mail.ru>
2020-12-09 23:28:18 +0500

    201209reader

--- reader.go
+++ reader.go
@@ -8,11 +8,22 @@ import (
 
 func main() {
 	r := strings.NewReader("Hello, Reader!")
+	g := strings.NewReader("1234 5678 9abcd")
 
-	b := make([]byte, 8)
+	b := make([]byte, 9)
 	for {
 		n, err := r.Read(b)
-		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+
+		fmt.Printf("n = %v; err = %v; b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Printf("Next!\n")
+
+		p, err := g.Read(b)
+		fmt.Printf("n = %v; err = %v; b = %v\n", p, err, b)
 		fmt.Printf("b[:n] = %q\n", b[:n])
 		if err == io.EOF {
 			break
