c18aa88409a35426f621a020bc2346c3ddfe9a15
ev <dmitry_dtv@mail.ru>
2020-12-18 14:21:56 +0500

    201218

---
+++ httpimage.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"os"
+	"time"
+
+	"GitHub.com/golang/freetype"
+	"GitHub.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goregular"
+	"golang.org/x/image/math/fixed"
+)
+
+func hexToRGBA(hex string) (color.RGBA, error) {
+	var (
+		rgba             color.RGBA
+		err              error
+		errInvalidFormat = fmt.Errorf("invalid")
+	)
+	rgba.A = 0xff
+	if hex[0] != '#' {
+		return rgba, errInvalidFormat
+	}
+	hexToByte := func(b byte) byte {
+		switch {
+		case b >= '0' && b <= '9':
+			return b - '0'
+		case b >= 'a' && b <= 'f':
+			return b - 'a' + 10
+		case b >= 'A' && b <= 'F':
+			return b - 'A' + 10
+		}
+		err = errInvalidFormat
+		return 0
+	}
+	switch len(hex) {
+	case 7:
+		rgba.R = hexToByte(hex[1])<<4 + hexToByte(hex[2])
+		rgba.G = hexToByte(hex[3])<<4 + hexToByte(hex[4])
+		rgba.B = hexToByte(hex[5])<<4 + hexToByte(hex[6])
+	case 4:
+		rgba.R = hexToByte(hex[1]) * 17
+		rgba.G = hexToByte(hex[2]) * 17
+		rgba.B = hexToByte(hex[3]) * 17
+	default:
+		err = errInvalidFormat
+	}
+	return rgba, err
+}
+
+func drawText(canvas *image.RGBA, text string) error {
+	var (
+		fgColor  image.Image
+		fontFace *truetype.Font
+		err      error
+		fontSize = 128.0
+	)
+	fgColor = image.White
+	fontFace, err = freetype.ParseFont(goregular.TTF)
+	fontDrawer := &font.Drawer{
+		Dst: canvas,
+		Src: fgColor,
+		Face: truetype.NewFace(fontFace, &truetype.Options{
+			Size:    fontSize,
+			Hinting: font.HintingFull,
+		}),
+	}
+	textBounds, _ := fontDrawer.BoundString(text)
+	xPosition := (fixed.I(canvas.Rect.Max.X) - fontDrawer.MeasureString(text)) / 2
+	textHeight := textBounds.Max.Y - textBounds.Min.Y
+	yPosition := fixed.I((canvas.Rect.Max.Y)-textHeight.Ceil())/2 + fixed.I(textHeight.Ceil())
+	fontDrawer.Dot = fixed.Point26_6{
+		X: xPosition,
+		Y: yPosition,
+	}
+	fontDrawer.DrawString(text)
+	return err
+}
+
+func main() {
+	initials := "LR"
+	size := 200
+	avatar, err := createAvatar(size, initials)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := fmt.Sprintf("out-%d.png", time.Now().Unix())
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	png.Encode(file, avatar)
+}
+func createAvatar(size int, initials string) (*image.RGBA, error) {
+	width, height := size, size
+	bgColor, err := hexToRGBA("#764abc")
+	if err != nil {
+		log.Fatal(err)
+	}
+	background := image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.Draw(background, background.Bounds(), &image.Uniform{C: bgColor},
+		image.Point{}, draw.Src)
+	drawText(background, initials)
+	return background, err
+}
--- httptest.go
+++ httptest.go
@@ -2,43 +2,66 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
-	"time"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	ctx := req.Context()
-	fmt.Println("server: hello handler started")
-	defer fmt.Println("server: hello handler ended")
+// func hello(w http.ResponseWriter, req *http.Request) {
+// 	ctx := req.Context()
+// 	fmt.Println("server: hello handler started")
+// 	defer fmt.Println("server: hello handler ended")
 
-	select {
-	case <-time.After(10 * time.Second):
-		fmt.Fprintf(w, "hello\n")
-	case <-ctx.Done():
+// 	select {
+// 	case <-time.After(10 * time.Second):
+// 		fmt.Fprintf(w, "hello\n")
+// 	case <-ctx.Done():
 
-		err := ctx.Err()
-		fmt.Println("server:", err)
-		internalError := http.StatusInternalServerError
-		http.Error(w, err.Error(), internalError)
-	}
-
-}
-
-func mainpage(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Response: this is main page\n")
-}
-
-// func headers(w http.ResponseWriter, req *http.Request) {
-// 	for name, headers := range req.Header {
-// 		for _, h := range headers {
-// 			fmt.Fprintf(w, "%v: %v\n", name, h)
-// 		}
+// 		err := ctx.Err()
+// 		fmt.Println("server:", err)
+// 		internalError := http.StatusInternalServerError
+// 		http.Error(w, err.Error(), internalError)
 // 	}
+
 // }
 
+// func mainpage(w http.ResponseWriter, req *http.Request) {
+// 	fmt.Fprintf(w, "Response: this is main page\n")
+// }
+
+// // func headers(w http.ResponseWriter, req *http.Request) {
+// // 	for name, headers := range req.Header {
+// // 		for _, h := range headers {
+// // 			fmt.Fprintf(w, "%v: %v\n", name, h)
+// // 		}
+// // 	}
+// // }
+
+// func main() {
+// 	http.HandleFunc("/hello", hello)
+// 	http.HandleFunc("/", mainpage)
+// 	// http.HandleFunc("/headers", headers)
+// 	http.ListenAndServe(":8090", nil)
+// }
+// create a handler struct
+type HttpHandler struct{}
+
+// implement `ServeHTTP` method on `HttpHandler` struct
+func (h HttpHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	// // create response binary data
+	// data := []byte("Hello World!") // slice of bytes
+	// // write `data` to response
+	// res.Write(data)
+	// write `Hello` using `io.WriteString` function
+	io.WriteString(res, "My")
+	// write `World` using `fmt.Fprint` function
+	fmt.Fprint(res, " Precious! ")
+	// write `❤️` using simple `Write` call
+	res.Write([]byte("❤️"))
+}
+
 func main() {
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/main", mainpage)
-	// http.HandleFunc("/headers", headers)
-	http.ListenAndServe(":8092", nil)
+	// create a new handler
+	handler := HttpHandler{}
+	// listen and serve
+	http.ListenAndServe(":9000", handler)
 }
