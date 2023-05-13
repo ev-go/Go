ba9774a5075eab0e3812b1040cb7253462c93133
Dmitry T <dmitry_dtv@mail.ru>
2023-05-14 04:14:55 +0500

    validator interface

--- tests/slices/slices_test.go
+++ tests/slices/slices_test.go
@@ -5,13 +5,72 @@ import (
 	"testing"
 )
 
-func TestSlices(t *testing.T) {
-	bracketRow := "[(())[]()[()]]"
-	ok, err := bracketValidator(bracketRow)
-	fmt.Println(ok, err)
+type Validator interface {
+	Put(element string)
+	Get() string
+	Pop()
 }
 
-func bracketValidator(bracketRow string) (bool, error) {
+func NewValidator() Validator {
+	return &Stack{
+		validationSlice: make([]string, 0, 10),
+	}
+}
+
+type Stack struct {
+	validationSlice []string
+}
+
+func (stack *Stack) Put(element string) {
+	stack.validationSlice = append(stack.validationSlice, element)
+}
+
+func (stack *Stack) Get() string {
+	return stack.validationSlice[len(stack.validationSlice)-1]
+}
+
+func (stack *Stack) Pop() {
+	stack.validationSlice = stack.validationSlice[:len(stack.validationSlice)-1]
+}
+
+func bracketValidatorPkg(inputRow string) bool {
+	validator := NewValidator()
+	rBrOp := "("
+	sBrOp := "["
+	rBrCl := ")"
+	sBrCl := "]"
+
+	var valueStr, prevBracket string
+	for _, value := range inputRow {
+		valueStr = string(value)
+		if (valueStr == rBrOp) || (valueStr == sBrOp) {
+			validator.Put(valueStr)
+		} else if valueStr == rBrCl {
+			prevBracket = validator.Get()
+			if prevBracket == rBrOp {
+				validator.Pop()
+			} else {
+				return false
+			}
+		} else if valueStr == sBrCl {
+			prevBracket = validator.Get()
+			if prevBracket == sBrOp {
+				validator.Pop()
+			} else {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func TestSlices(t *testing.T) {
+	bracketRow := "[(())[]()[()]]"
+	ok := bracketValidatorPkg(bracketRow)
+	fmt.Println(ok)
+}
+
+func bracketValidator(bracketRow string) bool {
 	validationSlice := make([]int32, 0, 100)
 	rBrOp := "("
 	sBrOp := "["
@@ -30,7 +89,7 @@ func bracketValidator(bracketRow string) (bool, error) {
 				pointer--
 				validationSlice = validationSlice[:pointer]
 			} else {
-				return false, nil
+				return false
 			}
 		} else if value == int32(sBrCl[0]) {
 			prevBr = validationSlice[pointer-1]
@@ -38,10 +97,10 @@ func bracketValidator(bracketRow string) (bool, error) {
 				pointer--
 				validationSlice = validationSlice[:pointer]
 			} else {
-				return false, nil
+				return false
 			}
 		}
 	}
 	verified = true
-	return verified, nil
+	return verified
 }
